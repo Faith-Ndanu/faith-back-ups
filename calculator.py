04cd3a84974a28213b89f7afe98ba1c13328c220
Faith-Ndanu <158448919+Faith-Ndanu@users.noreply.github.com>
2024-05-17 23:41:32 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,51 @@
+def add(x,y):
+    result=x+y
+    return result
+
+def subtract(y,z):
+    result = y-z
+    return result
+
+def multiply(a,b):
+    result = a*b
+    return result
+
+def divide(c,d):
+    result = c/d
+    return result
+
+def remainder(e,f):
+    result = e%f
+    return result
+
+def power_of(g,h):
+    result = g**h
+    return result
+
+def sum(*numbers):
+    total = 0
+    for number in numbers:
+        total+=number
+    return total    
+
+def multiply_many(*numbers):
+    product = 1
+    for number in numbers:
+        product*=number
+    return product
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
